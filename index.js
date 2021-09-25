12c48b4102e2eb504c04a0807d013079184d3190
Takashi <th2826@columbia.edu>
2021-09-25 14:37:59 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function isPalindrome(word) {
   return true;
 }
 
+/* LONGER VERSION
+function isPalindrome(word) {
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length/2; startIndex++){
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]){
+      // if the letters don't match, return false
+      return false
+    } 
+    // if all the letters match and reach the middle, return true
+    return true
+  }
+}
+*/
+
 /* Add your pseudocode here
   iterate from the beginning of the string to the middle of the string
   compare the letter we're iterating over to the corresponding letter at the end of the string
@@ -32,17 +48,3 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
-
-function isPalindrome(word) {
-  // iterate from the beginning of the string to the middle of the string
-  for (let startIndex = 0; startIndex < word.length/2; startIndex++){
-    // compare the letter we're iterating over to the corresponding letter at the end of the string
-    const endIndex = word.length - 1 - startIndex;
-    if (word[startIndex] !== word[endIndex]){
-      // if the letters don't match, return false
-      return false
-    } 
-    // if all the letters match and reach the middle, return true
-    return true
-  }
-}
